d08dd5dd70b6840c9d707646b29eebd50a248ccf
Puter <puterpark@me.com>
2023-12-01 16:24:48 +0900

    feat: 미들웨어 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import emailConfig from './config/emailConfig';
 import { validationSchema } from './config/validationSchema';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import * as process from "process";
+import { LoggerMiddleware } from "./logger/logger.middleware";
+import { UsersController } from "./users/users.controller";
+import { Logger2Middleware } from "./logger/logger2.middleware";
 
 @Module({
   imports: [
@@ -29,4 +32,12 @@ import * as process from "process";
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // 특정 모듈에 미들웨어 적용
+  configure(consumer: MiddlewareConsumer): any {
+    consumer
+      .apply(LoggerMiddleware, Logger2Middleware) // 미들웨어가 나열된 순서대로 적용
+      .exclude({ path: '/users', method: RequestMethod.GET }) // [GET] /users는 미들웨어 제외
+      .forRoutes(UsersController);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './util/swagger/setupSwagger';
 import { ValidationPipe } from "@nestjs/common";
+import { logger3 } from "./logger/logger3.middleware";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
+
+  app.use(logger3); // 미들웨어 전역 설정
 
   // swagger
   setupSwagger(app);
