167171203ca5f96222d1083acb9510d8e5833ed3
Puter <puterpark@me.com>
2023-12-01 14:18:49 +0900

    feat: 유효성 검사 파이프 사용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './util/swagger/setupSwagger';
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
 
   // swagger
   setupSwagger(app);
